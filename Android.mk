a0edcc3bc2a73877828f27d32d377852dc496860
Sabar <31942277+sabarop@users.noreply.github.com>
2025-03-17 10:32:06 +0700

    sdm660-common: Migrate mount point creation out of Android.mk
    Co-Authored-By: Michael Bestas <mikeNG@users.noreply.github.com>

--- Android.mk
+++ Android.mk
@@ -8,29 +8,7 @@ LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter tulip jasmine_sprout wayne clover lavender platina jason whyred,$(TARGET_DEVICE)),)
 
-$(shell mkdir -p $(TARGET_OUT_VENDOR)/firmware)
-
 include $(CLEAR_VARS)
-
-# A/B builds require us to create the mount points at compile time.
-# Just creating it for all cases since it does not hurt.
-FIRMWARE_MOUNT_POINT := $(TARGET_OUT_VENDOR)/firmware_mnt
-BT_FIRMWARE_MOUNT_POINT := $(TARGET_OUT_VENDOR)/bt_firmware
-DSP_MOUNT_POINT := $(TARGET_OUT_VENDOR)/dsp
-
-$(FIRMWARE_MOUNT_POINT):
-	@echo "Creating $(FIRMWARE_MOUNT_POINT)"
-	@mkdir -p $(TARGET_OUT_VENDOR)/firmware_mnt
-
-$(BT_FIRMWARE_MOUNT_POINT):
-	@echo "Creating $(BT_FIRMWARE_MOUNT_POINT)"
-	@mkdir -p $(TARGET_OUT_VENDOR)/bt_firmware
-
-$(DSP_MOUNT_POINT):
-	@echo "Creating $(DSP_MOUNT_POINT)"
-	@mkdir -p $(TARGET_OUT_VENDOR)/dsp
-
-ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_MOUNT_POINT) $(BT_FIRMWARE_MOUNT_POINT) $(DSP_MOUNT_POINT)
 
 METADATA_SYMLINKS := $(TARGET_ROOT_OUT)/metadata
 $(METADATA_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
