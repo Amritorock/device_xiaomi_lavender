meiser79/android_device_xiaomi_sdm660-common

sdm660-common: Set vendor init lib via soong config
Change-Id: I8f7e6b987bee58dbf09ca9ad7cfd5a7e99e19c3b

sdm660.mk
@@ -254,6 +254,8 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     ueventd.qcom.rc
 
+$(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):libinit_sdm660)
+
 # IRQ
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf

Amritorock/device_xiaomi_lavender

sdm660.mk
@@ -284,6 +284,8 @@ PRODUCT_PACKAGES += \
     IFAAService
 
 # Init
+$(call soong_config_set,libinit,vendor_init_lib,//$(COMMON_PATH):libinit_sdm660)
+
 PRODUCT_PACKAGES += \
     init.class_main.sh \
     init.goodix.sh \