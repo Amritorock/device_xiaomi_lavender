831dd917f0b8ce172653a952d13d656d61a77240
AmritoRock <amritokarmokar5i@gmail.com>
2026-04-02 02:32:16 +0600

    lavender: Checkout and cleanup overlay
    - from https://github.com/LineageOS/android_device_xiaomi_sdm660-common
    
    Signed-off-by: AmritoRock <amritokarmokar5i@gmail.com>

--- device.mk
+++ device.mk
@@ -225,7 +225,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
     LavenderCarrierConfigOverlay \
